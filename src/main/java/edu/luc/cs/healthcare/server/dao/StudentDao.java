5896ec511bb6927f794734eabc62ad402da44dd7
Michael Dotson <mdotson1@mdotson1-Aspire-7750G.(none)>
2013-10-21 16:27:29 -0500

    converted to asynh http client

--- src/main/java/edu/luc/cs/healthcare/server/dao/StudentDao.java
+++ src/main/java/edu/luc/cs/healthcare/server/dao/StudentDao.java
@@ -1,23 +1,12 @@
 package edu.luc.cs.healthcare.server.dao;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 import javax.ws.rs.core.UriBuilder;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-
+import com.ning.http.client.AsyncHttpClient;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.config.ClientConfig;
@@ -42,38 +31,34 @@ public enum StudentDao {
 	}
 
 	public void add(String id, Student student) {
-		HttpClient client = new DefaultHttpClient();
-		HttpPost post = new HttpPost("http://api.openkeyval.org/" + id);
+		AsyncHttpClient client = new AsyncHttpClient();
 		try {
-			List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1);
-			nameValuePairs.add(new BasicNameValuePair("data",
-					student.getFirstName() + " " + student.getLastName()));
-			post.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-
-			HttpResponse response = client.execute(post);
-			BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-
+			client.preparePost("http://api.openkeyval.org/" + id).addParameter("data", 
+					student.getFirstName() + " " + student.getLastName()).execute();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
 	public Student get(String id) {
-		final HttpClient client = new DefaultHttpClient();
-		final HttpGet request = new HttpGet("http://api.openkeyval.org/" + id);
+		AsyncHttpClient client = new AsyncHttpClient();
+		
+		String name = "";
+		try {
+			name = client.prepareGet("http://api.openkeyval.org/" + id)
+					.execute().get().getResponseBody();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		}
 
 		// Get the response
-		final String[] names;
-		
-		try {
-			HttpResponse response = client.execute(request);
-			BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-			final String line = rd.readLine();
-			names = line.split(" ");
-			return new Student(id, names[0], names[1]);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}
+		final String[] names = name.split(" ");
+		return new Student(id, names[0], names[1]);
 	}
 } 
